1ad2f191bd368bdf3b8bf704f44153532e549f1f
soloturn <soloturn@gmail.com>
2019-07-28 04:54:30 +0200

    python signal handling example
    copied from frank hofman, https://stackabuse.com/handling-unix-signals-in-python/

---
+++ signal-handling.py
@@ -0,0 +1,34 @@
+import signal
+import os
+import time
+
+def receiveSignal(signalNumber, frame):
+    print('Received:', signalNumber)
+    return
+
+if __name__ == '__main__':
+    # register the signals to be caught
+    signal.signal(signal.SIGHUP, receiveSignal)
+    signal.signal(signal.SIGINT, receiveSignal)
+    signal.signal(signal.SIGQUIT, receiveSignal)
+    signal.signal(signal.SIGILL, receiveSignal)
+    signal.signal(signal.SIGTRAP, receiveSignal)
+    signal.signal(signal.SIGABRT, receiveSignal)
+    signal.signal(signal.SIGBUS, receiveSignal)
+    signal.signal(signal.SIGFPE, receiveSignal)
+    #signal.signal(signal.SIGKILL, receiveSignal)
+    signal.signal(signal.SIGUSR1, receiveSignal)
+    signal.signal(signal.SIGSEGV, receiveSignal)
+    signal.signal(signal.SIGUSR2, receiveSignal)
+    signal.signal(signal.SIGPIPE, receiveSignal)
+    signal.signal(signal.SIGALRM, receiveSignal)
+    signal.signal(signal.SIGTERM, receiveSignal)
+
+    # output current process id
+    print('My PID is:', os.getpid())
+
+    # wait in an endless loop for signals 
+    while True:
+        print('Waiting...')
+        time.sleep(3)
+
